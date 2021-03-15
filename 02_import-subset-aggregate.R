7804f5c0d264b83f044e39eb7a80f87273fc628d
Michael Maguire <michaelqmaguire2@ufl.edu>
2021-03-15 15:47:21 -0400

    updated to remove ga

--- 02_import-subset-aggregate.R
+++ 02_import-subset-aggregate.R
@@ -187,3 +187,121 @@ dev.off()
 png(filename = "./plots/z-drugs-q1-2017-q1-2020-by-year-quarter-drug.png", width = 16, height = 9, units = "in", res = 1200)
 rx_by_year_quarter_drug
 dev.off()
+
+## -------------------------------- ##
+## ISSUE: GA MISSING DATA FOR 2018. ##
+## JHC SAID TO REMOVE FOR NOW.      ##
+## -------------------------------- ##
+
+## ------------------------------ ##
+## Step 6: Remove GA              ##
+## ------------------------------ ##
+
+sdud_2017_2020_z_flags_rm_ga <-
+  sdud_2017_2020_gennmes %>%
+  filter(
+    (eszcopiclone_flag == "1" | zaleplon_flag == "1" | zolpidem_flag == "1") & state != "ga"
+  )
+
+## -------------------------------------------------------------- ##
+## Step 7: Aggregate by state, year, quarter and sum, without GA. ##
+## -------------------------------------------------------------- ##
+
+ndc_gen_brand_names_rm_ga <- 
+  sdud_2017_2020_z_flags %>%
+  distinct(gennme_c, prodnme, ndc)
+
+sdud_2017_2020_zdrugs_rx_rm_ga <-
+  sdud_2017_2020_z_flags_rm_ga %>%
+  group_by(state, year, quarter, gennme_c, suppression_used) %>%
+  summarize(total_prescriptions = sum(number_of_prescriptions))
+
+sdud_2017_2020_zdrugs_rm_ga <- 
+  sdud_2017_2020_z_flags_rm_ga %>%
+  group_by(state, year, quarter, suppression_used) %>%
+  summarize(total_prescriptions = sum(number_of_prescriptions))
+
+## ----------------------------------------------------------- ##
+## Step 8: Plot number of prescriptions over time, without GA. ##
+## ----------------------------------------------------------- ##
+
+## Make plots for overall numbers.
+
+rx_by_year_quarter_rm_ga <-
+  ggplot(data = sdud_2017_2020_zdrugs_rm_ga) +
+  geom_col(aes(x = paste0(year, "-", quarter), y = total_prescriptions), fill = "forestgreen", alpha = 0.95) +
+  scale_y_continuous(labels = scales::comma) + 
+  theme_ipsum_rc(axis_title_just = "ct") +
+  ggtitle("Number of Prescriptions by Year and Quarter") +
+  xlab("Year-Quarter") +
+  ylab("Total Number of Prescriptions") +
+  theme(
+    axis.text.x = element_text(color = "black"),
+    axis.text.y = element_text(color = "black"),
+    axis.title.x = element_text(color = "black", size = 10),
+    axis.title.y = element_text(color = "black", size = 10),
+  ) +
+  coord_cartesian(expand = FALSE)
+
+rx_by_year_quarter_state_rm_ga <-
+  ggplot(data = sdud_2017_2020_zdrugs_rm_ga) +
+  geom_col(aes(x = paste0(year, "-", quarter), y = total_prescriptions, fill = state), alpha = 0.95) +
+  scale_fill_viridis_d() +
+  scale_y_continuous(labels = scales::comma) + 
+  theme_ipsum_rc(axis_title_just = "ct") +
+  ggtitle("Number of Prescriptions by Year, Quarter, and State") +
+  xlab("Year-Quarter") +
+  ylab("Total Number of Prescriptions") +
+  theme(
+    axis.text.x = element_text(color = "black", angle = 90, size = 10, hjust = 0.25, vjust = 0.25),
+    axis.text.y = element_text(color = "black"),
+    axis.title.x = element_text(color = "black", size = 10),
+    axis.title.y = element_text(color = "black", size = 10),
+    legend.position = "none"
+  ) +
+  facet_wrap(~state) +
+  coord_cartesian(expand = FALSE)
+
+## Make plots for individual drugs.
+
+rx_by_year_quarter_drug_rm_ga <- 
+  ggplot(data = sdud_2017_2020_zdrugs_rx_rm_ga) +
+  geom_col(aes(x = paste0(year, "-", quarter), y = total_prescriptions, fill = gennme_c)) +
+  scale_fill_viridis_d(direction = -1) +
+  scale_y_continuous(labels = scales::comma) +
+  theme_ipsum_rc(axis_title_just = "ct") +
+  ggtitle("Number of Prescriptions by Generic Name, Year, and Quarter") +
+  xlab("Year-Quarter") +
+  ylab("Total Number of Prescriptions") +
+  labs(fill = "Generic Name") +
+  theme(
+    axis.text.x = element_text(color = "black"),
+    axis.text.y = element_text(color = "black"),
+    axis.title.x = element_text(color = "black", size = 12),
+    axis.title.y = element_text(color = "black", size = 12)
+  ) +
+  coord_cartesian(expand = FALSE)
+
+## ------------------------------------------ ##
+## Step 9: Output data and plots, without GA  ##
+## ------------------------------------------ ##
+
+## Write out data to clean folder.
+
+write_csv(x = ndc_gen_brand_names_rm_ga, file = "./data/clean/z-drugs-generic-brand-ndc-no-ga.csv")
+write_csv(x = sdud_2017_2020_zdrugs_rm_ga, file = "./data/clean/z-drugs-q1-2017-q1-2020-year-quarter-state-no-ga.csv")
+write_csv(x = sdud_2017_2020_zdrugs_rx_rm_ga, file = "./data/clean/z-drugs-q1-2017-q1-2020-year-quarter-state-drug-no-ga.csv")
+
+## Save plots to the plots folder.
+
+png(filename = "./plots/z-drugs-q1-2017-q1-2020-by-year-quarter-no-ga.png", width = 16, height = 9, units = "in", res = 1200)
+rx_by_year_quarter_rm_ga
+dev.off()
+
+png(filename = "./plots/z-drugs-q1-2017-q1-2020-by-year-quarter-state-no-ga.png", width = 18, height = 12, units = "in", res = 1200)
+rx_by_year_quarter_state_rm_ga
+dev.off()
+
+png(filename = "./plots/z-drugs-q1-2017-q1-2020-by-year-quarter-drug-no-ga.png", width = 16, height = 9, units = "in", res = 1200)
+rx_by_year_quarter_drug_rm_ga
+dev.off()
